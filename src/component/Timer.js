98e0f1b2870df6652d3f548cc98f8ff8969cefbb
mahmoudhamdy <houdahamdy12@gmail.com>
2023-08-30 00:59:49 +0300

    finish project

--- src/component/Timer.js
+++ src/component/Timer.js
@@ -1,14 +1,10 @@
 import { useEffect,useState } from "react"
-// import useSound from "use-sound";
-// import wrong from "../sounds/wrong.mp3"
 
 export const Timer = ({setStop, questionNumber}) => {
     const [timer, setTimer] = useState(30)
-    // const [Loser] = useSound(wrong)
 
     useEffect(()=> {
       if (timer === 0 ) {
-        // Loser()
         setStop(true)
       }
       const intervel = setInterval(() => {
